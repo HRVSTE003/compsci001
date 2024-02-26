34a6c0a88845e4db67599413fbb36375da06b73c
HRVSTE003 <161364451+HRVSTE003@users.noreply.github.com>
2024-02-26 19:54:52 +0200

    Add files via upload

---
+++ energy.py
@@ -0,0 +1,14 @@
+# programme to convert values into energy 
+# Name: Stephanie Harvey 
+# 25 February 2024
+
+intput_str = input('Enter the value of m: ')
+m = int(intput_str)
+
+int_str2 = input('Enter the value of c: ')
+c = int(int_str2)
+
+energy = int(m*(c)**2)
+print('The value of energy, E, is:', energy)
+
+ 
